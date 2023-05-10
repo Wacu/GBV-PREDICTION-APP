3f50111ed9d73ce643e99a734d927e6ce99e9ea0
Wacu_Mutahi <28296097+Wacu@users.noreply.github.com>
2023-05-11 02:26:55 +0300

    Streamlit_app

---
+++ streamlit_app.py
@@ -0,0 +1,36 @@
+
+'''Lets start the concept'''
+import os
+
+import altair as alt
+import numpy as np
+import pandas as pd
+import pydeck as pdk
+import streamlit as st
+
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="GBV PREDICTION APP", page_icon=":woman:")
+
+# LOAD DATA ONCE
+@st.cache_resource
+def load_data():
+    path = "uber-raw-data-sep14.csv.gz"
+    if not os.path.isfile(path):
+        path = f"https://github.com/streamlit/demo-uber-nyc-pickups/raw/main/{path}"
+
+    data = pd.read_csv(
+        path,
+        nrows=100000,  # approx. 10% of data
+        names=[
+            "date/time",
+            "lat",
+            "lon",
+        ],  # specify names directly since they don't change
+        skiprows=1,  # don't read header since names specified directly
+        usecols=[0, 1, 2],  # doesn't load last column, constant value "B02512"
+        parse_dates=[
+            "date/time"
+        ],  # set as datetime instead of converting after the fact
+    )
+
+    return data
